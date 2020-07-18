733ff9e1f25128722bca3a4e36dac3fc7170b6eb
Roman <romires2000@gmail.com>
2020-07-19 02:21:53 +0300

    fix

---
+++ js/injection.js
@@ -0,0 +1,3 @@
+var elem = document.querySelector('.rQDP3').children[0].children[1].children[0].children[0].children[0].children[0].children[1].children[0].children[0].children[0].children[0].children[0];
+var src = elem.getAttribute('src');
+console.log(src)
--- js/main.js
+++ js/main.js
@@ -1,24 +1,18 @@
-var url = window.location.href;
-let context = {
-    id:'Dinsta',
-    title:'download image',
-    contexts:["page"],
-    onclick:function () {
-        console.log(url)
-        alert(url);
-    }
-};
+document.addEventListener('DOMContentLoaded',function () {
+    let img = document.getElementById('img');
+    let btn1 = document.getElementById('test');
+    btn1.addEventListener('click',()=>{
+        chrome.tabs.getSelected(null, (tab)=>{
+            chrome.tabs.executeScript(tab.id,{code:"alert(window.location.href)"})
+        })
+    }, false)
 
-
-let showContextMenu = function () {
-    console.log(window.location.href)
-    chrome.contextMenus.create(context);
-};
-
-let checkUrl = function (url) {
-    let u  = url.split('/');
-    return u
-}
-
-// showContextMenu();
-console.log(checkUrl(url));
+    let btn2 = document.getElementById('download');
+    btn2.addEventListener('click', ()=>{
+        chrome.tabs.getSelected(null, (tab)=>{
+            chrome.tabs.executeScript(tab.id, {file:"js/injection.js"}, function (responce) {
+                window.close()
+            })
+        })
+    })
+},false)
